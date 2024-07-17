c1302e5299d62807637db379cf70422316951709
Leon_lp9 <73760817+Leonlp9@users.noreply.github.com>
2024-07-18 01:10:43 +0200

    deltaspeed

--- lunikagame/script.js
+++ lunikagame/script.js
@@ -4,20 +4,26 @@ canvas.width = 400;
 canvas.height = 600;
 
 const fruits = [
-    { name: 'cherry', score: 10, size: 30, texture: "https://suikagame.com/public/res/raw-assets/ad/ad16ccdc-975e-4393-ae7b-8ac79c3795f2.png" },
-    { name: 'strawberry', score: 20, size: 40, texture: "https://suikagame.com/public/res/raw-assets/0c/0cbb3dbb-2a85-42a5-be21-9839611e5af7.png" },
-    { name: 'grape', score: 30, size: 60, texture: "https://suikagame.com/public/res/raw-assets/d0/d0c676e4-0956-4a03-90af-fee028cfabe4.png" },
-    { name: 'dekopon', score: 40, size: 80, texture: "https://suikagame.com/public/res/raw-assets/74/74237057-2880-4e1f-8a78-6d8ef00a1f5f.png" },
-    { name: 'orange', score: 50, size: 100, texture: "https://suikagame.com/public/res/raw-assets/13/132ded82-3e39-4e2e-bc34-fc934870f84c.png" },
-    { name: 'apple', score: 60, size: 120, texture: "https://suikagame.com/public/res/raw-assets/03/03c33f55-5932-4ff7-896b-814ba3a8edb8.png" },
-    { name: 'pear', score: 70, size: 140, texture: "https://suikagame.com/public/res/raw-assets/66/665a0ec9-6c43-4858-974c-025514f2a0e7.png" },
-    { name: 'peach', score: 80, size: 160, texture: "https://suikagame.com/public/res/raw-assets/84/84bc9d40-83d0-480c-b46a-3ef59e603e14.png" },
-    { name: 'pineapple', score: 90, size: 180, texture: "https://suikagame.com/public/res/raw-assets/5f/5fa0264d-acbf-4a7b-8923-c106ec3b9215.png" },
-    { name: 'melon', score: 100, size: 200, texture: "https://suikagame.com/public/res/raw-assets/56/564ba620-6a55-4cbe-a5a6-6fa3edd80151.png" },
-    { name: 'watermelon', score: 120, size: 220, texture: "https://suikagame.com/public/res/raw-assets/50/5035266c-8df3-4236-8d82-a375e97a0d9c.png" }
+    { name: 'cherry', score: 2, size: 30, texture: "https://suikagame.com/public/res/raw-assets/ad/ad16ccdc-975e-4393-ae7b-8ac79c3795f2.png" },
+    { name: 'strawberry', score: 4, size: 40, texture: "https://suikagame.com/public/res/raw-assets/0c/0cbb3dbb-2a85-42a5-be21-9839611e5af7.png" },
+    { name: 'grape', score: 6, size: 60, texture: "https://suikagame.com/public/res/raw-assets/d0/d0c676e4-0956-4a03-90af-fee028cfabe4.png" },
+    { name: 'dekopon', score: 8, size: 80, texture: "https://suikagame.com/public/res/raw-assets/74/74237057-2880-4e1f-8a78-6d8ef00a1f5f.png" },
+    { name: 'orange', score: 10, size: 100, texture: "https://suikagame.com/public/res/raw-assets/13/132ded82-3e39-4e2e-bc34-fc934870f84c.png" },
+    { name: 'apple', score: 12, size: 120, texture: "https://suikagame.com/public/res/raw-assets/03/03c33f55-5932-4ff7-896b-814ba3a8edb8.png" },
+    { name: 'pear', score: 14, size: 140, texture: "https://suikagame.com/public/res/raw-assets/66/665a0ec9-6c43-4858-974c-025514f2a0e7.png" },
+    { name: 'peach', score: 16, size: 160, texture: "https://suikagame.com/public/res/raw-assets/84/84bc9d40-83d0-480c-b46a-3ef59e603e14.png" },
+    { name: 'pineapple', score: 18, size: 180, texture: "https://suikagame.com/public/res/raw-assets/5f/5fa0264d-acbf-4a7b-8923-c106ec3b9215.png" },
+    { name: 'melon', score: 20, size: 200, texture: "https://suikagame.com/public/res/raw-assets/56/564ba620-6a55-4cbe-a5a6-6fa3edd80151.png" },
+    { name: 'watermelon', score: 22, size: 220, texture: "https://suikagame.com/public/res/raw-assets/50/5035266c-8df3-4236-8d82-a375e97a0d9c.png" }
 ];
 
 let fruitsArray = [];
+let score = 0;
+
+function addScore(amount) {
+    score += amount;
+    document.getElementById('score').innerText = score;
+}
 
 class Fruit {
     constructor(x, y, fruit, type) {
@@ -27,7 +33,7 @@ class Fruit {
         this.dy = 0;
         this.size = fruit.size;
         this.texture = fruit.texture;
-        this.gravity = 0.1;
+        this.gravity = 0.2;
         this.rotation = 0;
         this.rotationSpeed = 0;
         this.type = type;
@@ -44,13 +50,13 @@ class Fruit {
         ctx.restore();
     }
 
-    update() {
-        this.dy += this.gravity;
-        this.rotation += this.rotationSpeed;
+    update(deltaTime) {
+        this.dy += this.gravity * deltaTime;
+        this.rotation += this.rotationSpeed * deltaTime;
         this.rotation %= 360;
 
-        let nextX = this.x + this.dx;
-        let nextY = this.y + this.dy;
+        let nextX = this.x + this.dx * deltaTime;
+        let nextY = this.y + this.dy * deltaTime;
 
         if (nextX - this.size / 2 < 0) {
             this.dx = Math.abs(this.dx);
@@ -85,6 +91,7 @@ class Fruit {
             this.type = nextFruit.name;
             this.size = nextFruit.size;
             this.texture = nextFruit.texture;
+            addScore(nextFruit.score);
         }
     }
 }
@@ -147,11 +154,15 @@ function spawnFruit() {
     );
 }
 
-function animate() {
+let lastTime = 0;
+function animate(timestamp) {
+    const deltaTime = (timestamp - lastTime) / 16.6667; // Normalize the deltaTime
+    lastTime = timestamp;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     checkFruitCollisions();
     fruitsArray.forEach((fruit, index) => {
-        fruit.update();
+        fruit.update(deltaTime);
         if (fruit.y > canvas.height) {
             fruitsArray.splice(index, 1);
         }
@@ -159,5 +170,5 @@ function animate() {
     requestAnimationFrame(animate);
 }
 
-animate();
-setInterval(spawnFruit, 1000);
+animate(0);
+setInterval(spawnFruit, 100);
