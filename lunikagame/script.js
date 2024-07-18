6dffbc1b2b6f6badb6e8e1c56c7127907c27c830
Leon_lp9 <73760817+Leonlp9@users.noreply.github.com>
2024-07-18 16:30:15 +0200

    Upate

--- lunikagame/script.js
+++ lunikagame/script.js
@@ -4,17 +4,17 @@ canvas.width = 400;
 canvas.height = 700;
 
 let fruits = [
-    { name: 'cherry', score: 2, size: 30, texture: "https://suikagame.com/public/res/raw-assets/ad/ad16ccdc-975e-4393-ae7b-8ac79c3795f2.png", img: new Image() },
-    { name: 'strawberry', score: 4, size: 40, texture: "https://suikagame.com/public/res/raw-assets/0c/0cbb3dbb-2a85-42a5-be21-9839611e5af7.png", img: new Image() },
-    { name: 'grape', score: 6, size: 60, texture: "https://suikagame.com/public/res/raw-assets/d0/d0c676e4-0956-4a03-90af-fee028cfabe4.png", img: new Image() },
+    { name: 'cherry', score: 2, size: 35, texture: "https://suikagame.com/public/res/raw-assets/ad/ad16ccdc-975e-4393-ae7b-8ac79c3795f2.png", img: new Image() },
+    { name: 'strawberry', score: 4, size: 50, texture: "https://suikagame.com/public/res/raw-assets/0c/0cbb3dbb-2a85-42a5-be21-9839611e5af7.png", img: new Image() },
+    { name: 'grape', score: 6, size: 65, texture: "https://suikagame.com/public/res/raw-assets/d0/d0c676e4-0956-4a03-90af-fee028cfabe4.png", img: new Image() },
     { name: 'dekopon', score: 8, size: 80, texture: "https://suikagame.com/public/res/raw-assets/74/74237057-2880-4e1f-8a78-6d8ef00a1f5f.png", img: new Image() },
-    { name: 'orange', score: 10, size: 100, texture: "https://suikagame.com/public/res/raw-assets/13/132ded82-3e39-4e2e-bc34-fc934870f84c.png", img: new Image() },
-    { name: 'apple', score: 12, size: 120, texture: "https://suikagame.com/public/res/raw-assets/03/03c33f55-5932-4ff7-896b-814ba3a8edb8.png", img: new Image() },
-    { name: 'pear', score: 14, size: 140, texture: "https://suikagame.com/public/res/raw-assets/66/665a0ec9-6c43-4858-974c-025514f2a0e7.png", img: new Image() },
-    { name: 'peach', score: 16, size: 160, texture: "https://suikagame.com/public/res/raw-assets/84/84bc9d40-83d0-480c-b46a-3ef59e603e14.png", img: new Image() },
-    { name: 'pineapple', score: 18, size: 180, texture: "https://suikagame.com/public/res/raw-assets/5f/5fa0264d-acbf-4a7b-8923-c106ec3b9215.png", img: new Image() },
-    { name: 'melon', score: 20, size: 200, texture: "https://suikagame.com/public/res/raw-assets/56/564ba620-6a55-4cbe-a5a6-6fa3edd80151.png", img: new Image() },
-    { name: 'watermelon', score: 22, size: 220, texture: "https://suikagame.com/public/res/raw-assets/50/5035266c-8df3-4236-8d82-a375e97a0d9c.png", img: new Image() }
+    { name: 'orange', score: 10, size: 110, texture: "https://suikagame.com/public/res/raw-assets/13/132ded82-3e39-4e2e-bc34-fc934870f84c.png", img: new Image() },
+    { name: 'apple', score: 12, size: 130, texture: "https://suikagame.com/public/res/raw-assets/03/03c33f55-5932-4ff7-896b-814ba3a8edb8.png", img: new Image() },
+    { name: 'pear', score: 14, size: 150, texture: "https://suikagame.com/public/res/raw-assets/66/665a0ec9-6c43-4858-974c-025514f2a0e7.png", img: new Image() },
+    { name: 'peach', score: 16, size: 180, texture: "https://suikagame.com/public/res/raw-assets/84/84bc9d40-83d0-480c-b46a-3ef59e603e14.png", img: new Image() },
+    { name: 'pineapple', score: 18, size: 200, texture: "https://suikagame.com/public/res/raw-assets/5f/5fa0264d-acbf-4a7b-8923-c106ec3b9215.png", img: new Image() },
+    { name: 'melon', score: 20, size: 230, texture: "https://suikagame.com/public/res/raw-assets/56/564ba620-6a55-4cbe-a5a6-6fa3edd80151.png", img: new Image() },
+    { name: 'watermelon', score: 22, size: 260, texture: "https://suikagame.com/public/res/raw-assets/50/5035266c-8df3-4236-8d82-a375e97a0d9c.png", img: new Image() }
 ];
 
 function preloadImages() {
@@ -26,7 +26,7 @@ preloadImages()
 
 let fruitsArray = [];
 let score = 0;
-let lastX = 200;
+let lastX = canvas.width / 2;
 
 function addScore(amount) {
     score += amount;
@@ -85,6 +85,12 @@ class Fruit {
 
         this.x = nextX;
         this.y = nextY;
+
+        if (newDroppingFruit !== this) {
+            if (this.y + this.size / 2 < 115 && this.dy < 0) {
+                fruitsArray = [];
+            }
+        }
 
         this.draw();
     }
@@ -165,6 +171,8 @@ function spawnFruit() {
 canvas.addEventListener('mousemove', (event) => {
     if (newDroppingFruit) {
         newDroppingFruit.x = event.clientX - canvas.getBoundingClientRect().left;
+        newDroppingFruit.update(0)
+        lastX = newDroppingFruit.x;
     }
 });
 
@@ -182,6 +190,8 @@ canvas.addEventListener('click', (event) => {
 canvas.addEventListener('touchmove', (event) => {
     if (newDroppingFruit) {
         newDroppingFruit.x = event.touches[0].clientX - canvas.getBoundingClientRect().left;
+        newDroppingFruit.update(0)
+        lastX = newDroppingFruit.x;
     }
 });
 
@@ -212,8 +222,8 @@ function animate(timestamp) {
     ctx.beginPath();
     ctx.moveTo(0, 100);
     ctx.lineTo(50, 50);
-    ctx.lineTo(350, 50);
-    ctx.lineTo(400, 100);
+    ctx.lineTo(canvas.width - 50, 50);
+    ctx.lineTo(canvas.width, 100);
     ctx.closePath();
     ctx.fillStyle = '#e8d5ad';
     ctx.fill();
@@ -222,14 +232,28 @@ function animate(timestamp) {
     ctx.beginPath();
     ctx.moveTo(5, 110);
     ctx.lineTo(50, 50);
-    ctx.lineTo(350, 50);
-    ctx.lineTo(395, 110);
-    ctx.lineTo(395, canvas.height - 5);
+    ctx.lineTo(canvas.width - 50, 50);
+    ctx.lineTo(canvas.width - 5, 110);
+    ctx.lineTo(canvas.width - 5, canvas.height - 5);
     ctx.lineTo(5, canvas.height - 5);
     ctx.closePath();
     ctx.lineWidth = 20;
     ctx.strokeStyle = '#eed37b';
     ctx.stroke();
+
+    if (newDroppingFruit) {
+        //draw line from at x=lastX y=50 to y=canvas.height/2 The line loses transparency the closer it gets to the bottom
+        const gradient = ctx.createLinearGradient(lastX, 50, lastX, canvas.height / 2);
+        gradient.addColorStop(0, 'rgb(255,255,255)');
+        gradient.addColorStop(1, 'rgba(255,255,255,0)');
+        ctx.beginPath();
+        ctx.moveTo(lastX, 50);
+        ctx.lineTo(lastX, canvas.height / 1.2);
+        ctx.strokeStyle = gradient;
+        ctx.lineWidth = 4;
+        ctx.stroke();
+
+    }
 
     checkFruitCollisions();
     fruitsArray.forEach((fruit, index) => {
